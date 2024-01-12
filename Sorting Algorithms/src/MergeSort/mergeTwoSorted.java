9b7ffd3283ccf54ee2657636b56869fdb706f364
Akshayredekar07 <akshayredekar1414@gmail.com>
2024-01-12 10:25:30 +0530

    Added  efficient approach of merge tw sorted arrays🚀

--- Sorting Algorithms/src/MergeSort/mergeTwoSorted.java
+++ Sorting Algorithms/src/MergeSort/mergeTwoSorted.java
@@ -10,6 +10,7 @@ public class mergeTwoSorted {
 
     }
 
+    //Naive approach
     static void printMergedArrays(int[] a, int[] b){
         int[] c = new int[a.length+b.length];
         for (int i = 0; i < a.length; i++) {
@@ -21,6 +22,28 @@ public class mergeTwoSorted {
         Arrays.sort(c);
         for (int i = 0; i < (a.length + b.length); i++) {
             System.out.print(c[i]+" ");
+        }
+    }
+
+    //efficient approach
+    public static void merge(int[]a, int[]b){
+        int i = 0, j = 0;
+        while(i < a.length && j < b.length){
+            if(a[i] <= b[j]){
+                System.out.print(a[i] + " ");
+                i++;
+            }else{
+                System.out.print(b[j] + " ");
+                j++;
+            }
+        }
+        while (i < a.length){
+            System.out.print(a[i] + " ");
+            i++;
+        }
+        while (j < b.length){
+            System.out.print(b[j] + " ");
+            j++;
         }
     }
 
